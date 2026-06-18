80c717d0e78a523c31e4e0c8199fbfb01d4b562f
Lucian <lucian.seremet04@gmail.com>
2026-06-18 15:46:04 +0300

    Am schimbat titlul

--- app.py
+++ app.py
@@ -350,5 +350,5 @@ def init_db():
 
 if __name__ == "__main__":
     init_db()
-    # debug=True este util pentru prezentare/dezvoltare; dezactivați în producție.
+    
     app.run(debug=True, host="0.0.0.0", port=5000)
